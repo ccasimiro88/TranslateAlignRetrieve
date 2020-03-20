e8ec76430f6d762f6a24e66cca1f029e22126640
casimiro <casimiro.pio.carrino@veue01.tsc.upc.edu>
2020-03-20 18:09:07 +0100

    set source and target language with default values

--- src/tar/translate_squad.sh
+++ src/tar/translate_squad.sh
@@ -5,18 +5,13 @@ source ${ENV_DIR}/bin/activate
 
 SQUAD_FILE=$1
 OUTPUT_DIR=$2
-LANG_SRC=$3
-LANG_TGT=$4
-RETRIEVE_FROM_ALIGNMENT=$5
+RETRIEVE_FROM_ALIGNMENT=$3
 
-if [[ -z "$3" ]] && [[ -z "$4" ]]
-  then
-  LANG_SRC=en
-  LANG_TGT=es
-fi
+LANG_SRC=en
+LANG_TGT=es
 
 TRANSLATE_RETRIEVE_DIR=${SCRIPT_DIR}/src/retrieve
-if [[ -z "$5" ]]; then
+if [[ -z "$3" ]]; then
     python ${TRANSLATE_RETRIEVE_DIR}/translate_retrieve_squad.py \
            -squad_file  ${SQUAD_FILE} \
            -lang_source ${LANG_SRC} \
